dbaf777eb7a8b238489d01c4a8000612dac8eba8
Godwin Idemudia <godwinide@gmail.com>
2023-04-24 11:00:11 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors")
+const app = express();
+
+require("dotenv").config();
+
+app.use(cors())
+app.use(express.static("./build"));
+
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build/index.html"))
+})
+
+const PORT = 6959;
+
+app.listen(PORT, () => console.log(`server start on port ${PORT}`));
---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useRef } from 'react';
+
+function App() {
+  const audio = useRef();
+  const bgAudio = useRef();
+  const preloadWrap = useRef();
+  const startButton = useRef();
+  const overlay = useRef();
+
+  const handleTwwet = () => {
+    const key = (Math.random()*100000).toString(36);
+    const tweet = `•˚ I’m searching for the Gates of Atlantis. •˚%0A%0A•˚ @LegendOfSirena has given the key ${key} •˚`;
+    window.open(`https://twitter.com/intent/tweet?text=${tweet}&url=${"https://website.com"}`, '_blank');
+  }
+
+  const handlePlay = () => {
+    startButton.current.style.display = "none";
+    audio.current.play();
+    overlay.current.style.display = "none";
+    setTimeout(()=> {
+      handleEnd();
+    }, 10000)
+  }
+
+  const handleEnd = () => {
+    preloadWrap.current.style.display = "none";
+    // bgAudio.current.play();
+  }
+
+  return (
+    <div className='main'>
+      <div className="preloader" ref={preloadWrap}>
+        <button
+          ref={startButton}
+          onClick={handlePlay}
+          >Click To Start
+        </button>
+        <audio 
+          src='/sirena_buildup_loopable.wav' 
+          ref={audio}
+          onEnded={handleEnd}
+          preload='auto'
+        />
+        <audio 
+          src='/sirena_buildup_loopable.wav.wav' 
+          ref={bgAudio}
+          loop
+          preload='auto'
+        />
+        <div className='overlay' ref={overlay}/>
+        
+      </div>
+      <button onClick={handleTwwet}>
+        Enter the Golden Age
+      </button>
+    </div>
+  )
+}
+
+export default App
